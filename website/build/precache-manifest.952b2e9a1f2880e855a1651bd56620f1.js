9008cb7f31563d3fedcc1e517e7fed62182e5671
voidful <voidful.stack@gmail.com>
2020-07-12 23:18:31 +0800

    remove iframe

---
+++ website/build/precache-manifest.952b2e9a1f2880e855a1651bd56620f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cea85d15825f88e132292c13ff4f37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e1b47a0bfa7f593d712",
+    "url": "/static/css/main.94b407f2.chunk.css"
+  },
+  {
+    "revision": "2071a31950363872a982",
+    "url": "/static/js/2.5599bc47.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.5599bc47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1b47a0bfa7f593d712",
+    "url": "/static/js/main.1b8eba0b.chunk.js"
+  },
+  {
+    "revision": "2224633fa0f49627909c",
+    "url": "/static/js/runtime-main.b007f7b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
